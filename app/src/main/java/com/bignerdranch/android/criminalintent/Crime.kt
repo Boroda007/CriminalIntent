8f18746eae77912fe383e617d9f8714088363916
Oleg Maksakov <maksakov.ol@yandex.ru>
2021-11-14 22:31:09 +0300

    Инициализация приложения. Добавлена главная Activity с фрагментом.

---
+++ app/src/main/java/com/bignerdranch/android/criminalintent/Crime.kt
@@ -0,0 +1,10 @@
+package com.bignerdranch.android.criminalintent
+
+import java.util.*
+
+data class Crime(
+    val id: UUID = UUID.randomUUID(),
+    var title: String = "",
+    var data: Date = Date(),
+    var isSolved: Boolean = false
+)
